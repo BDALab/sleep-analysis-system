6192d258e3d2de2d5bdf0b1db7e020fe3461ab3f
Jiri Mekyska <j.mekyska@gmail.com>
2021-01-01 17:16:37 +0100

    Cherry-pick from master - bugfixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,8 +147,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = f'{BASE_DIR}/../static/'
 
 LOGGING = {
     'version': 1,
@@ -204,5 +204,5 @@ LOGGING = {
     }
 }
 
-MEDIA_ROOT = 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
